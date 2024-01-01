8423ab8d13296e51baf3a9a2622d96fb0fa7c83e
TanmayGharat1505 <142776418+TanmayGharat1505@users.noreply.github.com>
2024-01-01 10:51:48 +0530

    Add files via upload

---
+++ swapping_values.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() 
+{
+    int a, b, c;
+
+    printf("Enter the value of a");
+    scanf("%d",&a);
+
+    printf("Enter the value of b");
+    scanf("%d",&b);
+
+    c=a;
+    a=b;
+    b=c;
+
+    printf ("a=%d,b=%d",a,b);
+
+    return 0;
+}
